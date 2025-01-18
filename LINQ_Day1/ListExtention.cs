f2be1e7f600b4d0717a9166c59c813ea291ab749
ArwaAmmosh <arwaahmedamosh@gmail.com>
2025-01-18 11:55:01 +0200

    Apply Extention method to List Data type to simulate Where in LINQ

---
+++ LINQ_Day1/ListExtention.cs
@@ -0,0 +1,21 @@
+﻿
+using System.Diagnostics;
+
+namespace LINQ_Day1
+{
+    public static class ListExtention
+    {
+        public static List<int> Condition(this List<int> numbers , Func<int,bool> func)
+        {
+            List<int> result = new List<int>();
+            for(int i=0; i<numbers.Count; i++)
+            {
+                if (func.Invoke(numbers[i]))
+                {
+                    result.Add(numbers[i]);
+                }
+            }
+            return result;
+        }
+    }
+}
---
+++ LINQ_Day1/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace LINQ_Day1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> list = [1,2,3,6,5,7,8,9,10,12];
+            var result= list.Condition(args => args %2 ==0);
+            foreach(int item in result)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}

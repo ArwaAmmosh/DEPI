c087776c0d3dbddb2dd8dedd38d809d31c3f57b9
ArwaAmmosh <arwaahmedamosh@gmail.com>
2025-01-20 20:55:32 +0200

    Last or default & distinct & order by & single or default &first or default & first

--- LINQ_Day1/ListExtention.cs
+++ LINQ_Day1/ListExtention.cs
@@ -1,13 +1,15 @@
 ﻿
+using System.Data;
 using System.Diagnostics;
 
 namespace LINQ_Day1
 {
     public static class ListExtention
     {
-        public static List<int> Condition(this List<int> numbers , Func<int,bool> func)
+        #region Where
+        public static List<T> Condition<T>(this List<T> numbers , Func<T,bool> func)
         {
-            List<int> result = new List<int>();
+            List<T> result = new List<T>();
             for(int i=0; i<numbers.Count; i++)
             {
                 if (func.Invoke(numbers[i]))
@@ -17,5 +19,104 @@ namespace LINQ_Day1
             }
             return result;
         }
+        #endregion
+        #region first
+        public static T FirstMatch<T>(this List<T> numbers, Func<T, bool> func)
+        {
+           
+                for (int i = 0; i < numbers.Count; i++)
+                {
+                    if (func.Invoke(numbers[i]))
+                    {
+                        return numbers[i];
+                    }
+                }
+            throw new InvalidOperationException("No Matching element found in the list");
+        }
+        #endregion
+        #region first or default
+        public static T? FirstOrDefaultMatch<T>(this List<T> numbers, Func<T, bool> func) 
+        { 
+
+            for (int i = 0; i < numbers.Count; i++)
+            {
+                if (func.Invoke(numbers[i]))
+                {
+                    return numbers[i];
+                }
+            }
+            return default;
+        }
+        #endregion
+        #region single or default
+        public static T? SingleOrDefaultMatch<T>(this List<T> numbers, Func<T, bool> func)
+        {
+            var result = default(T);
+            bool found = false;
+            for (int i = 0; i < numbers.Count; i++)
+            {
+                if (func.Invoke(numbers[i]))
+                {
+                    if (found)
+                        throw new InvalidOperationException("sequence contain more than one element");
+                    found = true;
+                    result = numbers[i];
+                }
+            }
+            return result;
+        }
+
+        #endregion
+        #region order by 
+        public static List<T> OrderBY<T>(this List<T> numbers,Func<T,T,bool>fun)
+        {
+
+            for (int i = 0; i < numbers.Count-1; i++)
+            {
+                for(int j = i+1; j< numbers.Count; j++)
+                {
+                    if (fun.Invoke(numbers[i], numbers[j]))
+                    {
+                        var temp = numbers[i];
+                        numbers[i] = numbers[j];
+                        numbers[j] = temp;
+                        
+                    }
+                }
+                
+            }
+         return numbers;
+        }
+
+        #endregion
+        #region distinct
+        public static List<T> Distinct<T>(this List<T> numbers)
+        {
+            HashSet<T> set = new HashSet<T>();
+            List<T> list = new List<T>();
+            for (int i = 0; i < numbers.Count; i++)
+            {
+                if (set.Add(numbers[i]))
+                {
+                    list.Add(numbers[i]);
+                }
+            }
+            return list;
+        }
+        #endregion
+        #region Last or default
+        public static T? LastOrDefaultMatch<T>(this List<T> numbers, Func<T, bool> func)
+        {
+
+            for (int i = numbers.Count-1; i > -1; i--)
+            {
+                if (func.Invoke(numbers[i]))
+                {
+                    return numbers[i];
+                }
+            }
+            return default;
+        }
+        #endregion
     }
 }
--- LINQ_Day1/Program.cs
+++ LINQ_Day1/Program.cs
@@ -4,9 +4,27 @@
     {
         static void Main(string[] args)
         {
-            List<int> list = [1,2,3,6,5,7,8,9,10,12];
+            List<int> list = [1,2,3,6,5,7,8,9,10,5,50,2,3,12];
             var result= list.Condition(args => args %2 ==0);
             foreach(int item in result)
+            {
+                Console.WriteLine(item);
+            }
+            var result2 = list.FirstMatch(args => args % 2 == 0);
+            Console.WriteLine(result2);
+            var result3 =list.FirstOrDefaultMatch(args => args % 20 == 0);
+            Console.WriteLine(result3);
+            var result4 =list.SingleOrDefaultMatch(args => args % 20 == 0);
+            Console.WriteLine(result3);
+            var list2 = list.OrderBY((a,b)=>a>b);
+            foreach (int item in list2)
+            {
+                Console.WriteLine(item);
+            }
+            var result5=list.LastOrDefaultMatch(args=> args % 2 == 0);
+            Console.WriteLine(result5);
+            var list3 = list.Distinct();
+            foreach (int item in list3)
             {
                 Console.WriteLine(item);
             }

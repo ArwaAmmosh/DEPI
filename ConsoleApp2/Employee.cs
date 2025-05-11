dacf7ed534986c73b96ef2528c080e450872eb6d
Arwa Ammosh <112775694+ArwaAmmosh@users.noreply.github.com>
2025-05-11 23:36:22 +0300

    Add files via upload

---
+++ ConsoleApp2/Employee.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    public class Employee
+    {
+        public Employee(int employeeId, string name, int age, decimal salary, string hrId, double bounse, int nLeaves)
+        {
+            Id = employeeId;
+            Name = name;
+            Age = age;
+            Salary = salary;
+            HrId = hrId;
+            Bounse = bounse;
+            NLeaves = nLeaves;
+        }
+
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public decimal Salary { get; set; }
+        public string HrId { get; set; }
+        public double Bounse { get; set; }
+        public int NLeaves { get; set; }
+        public bool IsDeleted { get; set; }=false;
+        public DateTime Createdon { get; set; }= DateTime.Now;
+        public void Print()
+        {
+            Console.WriteLine($"Employee - ID :{this.Id}  Name:{this.Name} Age:{this.Age} Salary:{this.Salary} HRId:{this.HrId} Bounes:{this.Bounse} NofLeaves:{this.NLeaves} Created on:{this.Createdon} IsDeleted:{IsDeleted}");
+        }
+        public void TogelStatusEmployee()
+        {
+            this.IsDeleted = !this.IsDeleted;
+            Console.WriteLine("Done Sucessfully!");
+        }
+        public void printExistingEmployees()
+        {
+           
+                if (this.IsDeleted)
+                Console.WriteLine("Not Existing");
+                else
+                {
+                    Console.WriteLine($"Employee - ID :{this.Id}  Name:{this.Name} Age:{this.Age} Salary:{this.Salary} HRId:{this.HrId} Bounes:{this.Bounse} NofLeaves:{this.NLeaves} Created on:{this.Createdon} ");
+
+                }
+            
+        }
+    }
+
+    public static class EmployeeNonfunctionality
+    {
+        public static void printEmployess(Employee[] emp)
+        {
+            foreach (Employee employee in emp)
+            {
+                Console.WriteLine($"Employee - ID :{employee.Id}  Name:{employee.Name} Age:{employee.Age} Salary:{employee.Salary} HRId:{employee.HrId} Bounes:{employee.Bounse} NofLeaves:{employee.NLeaves} Created on:{employee.Createdon} ");
+            }
+        }
+        public static int findIndex(Employee[] emps,int id)
+        {
+            for (int i= 0;i < emps.Length;i++)
+            {
+                if (emps[i].Id == id)
+                {
+                    return i;
+                }
+            }
+            return -1;
+        }
+       public static void FillData(int num)
+        {
+            int[] arr = new int[10];
+            int[] arr2;
+            int index2 = 11;
+            int index = 0;
+            if (index == arr.Length)
+            {
+                arr2 = new int[20];
+                Array.Copy(arr, arr2, arr.Length);
+                arr2[index] = num;
+                index++;
+            }
+            else
+            {
+                arr[index] = num;
+                index++;
+            }
+        }
+    }
+}
---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,87 @@
+﻿namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.Write("Enter Number of Employees :");
+            //int size=int.Parse(Console.ReadLine());
+            //Employee[] employees = new Employee[size];
+            //for (int i = 0; i < size; i++)
+            //{
+            //    Console.Write("Enter Employee Id: ");
+            //    int Id=int.Parse(Console.ReadLine());
+            //    Console.Write("Enter Employee Name: ");
+            //    string Name= Console.ReadLine();
+            //    Console.Write("Enter Employee Age: ");
+            //    int Age = int.Parse(Console.ReadLine());
+            //    Console.Write("Enter Employee Salary:");
+            //    decimal Salary = decimal.Parse(Console.ReadLine());
+            //    Console.Write("Enter HrId: ");
+            //    string HrId = Console.ReadLine();
+            //    Console.Write("Enter Employee Bounse:");
+            //    double Bounse = double.Parse(Console.ReadLine());
+            //    Console.Write("Enter N of Leaves : ");
+            //    int NLeaves = int.Parse(Console.ReadLine());
+            //    employees[i] = new Employee(Id,Name,Age,Salary,HrId,Bounse,NLeaves);
+
+            //}
+            //foreach (Employee employee in employees)
+            //{
+            //    employee.Print();
+
+            //}
+            ////toggle status
+            //Console.WriteLine("Toggle Employee Status:");
+            ////Enter Id 
+            //Console.Write("Enter Employee Id:");
+            ////check Id ==>Index 
+            //int index = EmployeeNonfunctionality.findIndex(employees, int.Parse(Console.ReadLine()));
+            /////Arrr[Id]
+            //if (index == -1)
+            //{
+            //    Console.WriteLine("Employee Not Exist or Incorrect Id");
+            //}
+            //else 
+            //{
+            //    employees[index].TogelStatusEmployee();
+            //}
+            /////
+            //foreach (Employee employee in employees) 
+            //{ 
+            //    employee.printExistingEmployees();
+            //}
+            int[] arr= new int[10];
+            Console.WriteLine("Enter Number -1 To Exit loop");
+            while (true)
+            {
+                int index = 0;
+                Console.Write("Enter number:");
+                int x= int.Parse(Console.ReadLine());
+                if (x == -1)
+                {
+                    break;
+                }
+                else 
+                {
+                    if (index == arr.Length)
+                    {
+                        int[] arr2= new int[20];
+                        Array.Copy(arr,arr2,arr.Length);
+                        arr2[index] = x;
+                        index++;
+
+                    }
+                    else 
+                    { 
+                        arr[index] = x;
+                        index++;
+                    }
+                }
+
+            }
+            
+
+        }
+    }
+}
